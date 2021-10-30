69ed5e4c2bbd222f4f4b499fafe5163f5cdaca9a
Adorable trooper <67198857+adorabletrooper@users.noreply.github.com>
2021-10-30 20:10:01 +0530

    Final Commit

--- program2/main.java
+++ program2/main.java
@@ -1,5 +1,11 @@
 package program2;
-
+import java.util.Scanner;
 public class main {
-    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = in.nextInt();
+        int dis = num < 0 ? (num - num - num) : num;
+        System.out.println(dis);
+    }
 }
