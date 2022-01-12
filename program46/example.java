1a4af364df5ddb7de797ea84293c40c753fbf212
Adorable trooper <67198857+adorabletrooper@users.noreply.github.com>
2022-01-12 16:23:52 +0530

    Add

---
+++ program46/example.java
@@ -0,0 +1,16 @@
+package program46;
+
+public class example {
+    public static void main(String args[]) {
+    String string = "Spring is beautiful but so is winter";
+    String word = "a";
+    String temp[] = string.split(" ");
+    int count = 0;
+    for (int i = 0; i < temp.length; i++) {
+    if (word.equals(temp[i]))
+    count++;
+    }
+    System.out.println("The string is: " + string);
+    System.out.println("The letter " + word + " occurs " + count + " times in the above string");
+    }
+    }
